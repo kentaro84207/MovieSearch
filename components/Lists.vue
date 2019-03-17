b95d32bf4880b3c7db4b896cad58c94af61bbc15
kentaro84207 <kentaro84207@gmail.com>
2019-03-17 12:56:35 +0900

    add image

--- components/Lists.vue
+++ components/Lists.vue
@@ -5,6 +5,9 @@
       <ul>
         <li v-for="(item, index) in items" v-cloak :key="index">
           {{ item.title }}
+          <img
+            :src="'https://image.tmdb.org/t/p/original' + item.poster_path"
+          />
         </li>
       </ul>
     </div>
@@ -13,6 +16,11 @@
 
 <script>
 export default {
+  // data() {
+  //   return {
+  //     imgUrl:
+  //   }
+  // },
   computed: {
     items() {
       return this.$store.state.items.results
