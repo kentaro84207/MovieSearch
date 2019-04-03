82b07cec219644da86349aab78a636e1c2b02b8a
kentaro84207 <kentaro84207@gmail.com>
2019-04-03 22:29:08 +0900

    improve action

--- store/actions.js
+++ store/actions.js
@@ -3,23 +3,17 @@ import axios from 'axios'
 const BASE_URL = 'https://api.themoviedb.org/3/'
 
 export default {
-  getData({ commit }, payload) {
+  async getData({ commit }, payload) {
     commit('changeloadingState')
-    console.log(payload)
-    axios
-
-      .get(BASE_URL + payload.addUrl, {
+    try {
+      const res = await axios.get(BASE_URL + payload.addUrl, {
         params: payload.params
       })
-      .then(response => {
-        commit('setItems', response.data)
-      })
-      .catch(e => {
-        console.error('error:', e)
-      })
-      .then(function() {
-        commit('changeloadingState')
-      })
+      commit('setItems', res.data)
+    } catch (e) {
+      console.error('error:', e)
+    }
+    commit('changeloadingState')
   },
   changeDialog({ commit }) {
     commit('changeDialogState')
